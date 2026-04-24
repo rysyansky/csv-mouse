f22c6f773adcb8cc2e705495c1e65eddeaaeb795
rysyansky <rashubin.528@gmail.com>
2026-04-24 14:51:58 +0300

    Fixed: separator adding in the end of each row

--- csv-mouse.hpp
+++ csv-mouse.hpp
@@ -7,16 +7,15 @@
 class csv_mouse {
 public:
     inline csv_mouse(int columns, char separator = ',');
-    inline csv_mouse(const std::vector<std::string>& row, char separator = ',');
+    inline csv_mouse(const std::vector<std::string> &row, char separator = ',');
     inline ~csv_mouse();
 
-    inline void add_row(const std::vector<std::string>& row);
+    inline void add_row(const std::vector<std::string> &row);
     inline void save_table(std::string filename) const;
 private:
     std::vector<std::vector<std::string>> table;
     char sep;
-    inline void check_row_size(const std::vector<std::string>& row);
-    
+    inline void check_row_size(const std::vector<std::string> &row);
 };
 
 csv_mouse::csv_mouse(int columns, char separator) {
@@ -24,7 +23,7 @@ csv_mouse::csv_mouse(int columns, char separator) {
     sep = static_cast<char>(separator);
 }
 
-csv_mouse::csv_mouse(const std::vector<std::string>& row, char separator) :
+csv_mouse::csv_mouse(const std::vector<std::string> &row, char separator) :
     csv_mouse(row.size(), separator) {
         add_row(row);
     }
@@ -33,7 +32,7 @@ csv_mouse::~csv_mouse() {
     table.clear();
 }
 
-void csv_mouse::add_row(const std::vector<std::string>& row) {
+void csv_mouse::add_row(const std::vector<std::string> &row) {
     check_row_size(row);
     for (int i = 0; i < row.size(); ++i) {
         table[i].push_back(row[i]);
@@ -46,9 +45,16 @@ void csv_mouse::save_table(std::string filename) const {
         throw std::runtime_error("Can't open file: " + filename);
     }
 
+    if (table.empty() || table[0].empty()) {
+        throw std::runtime_error("Table is empty, nothing to save.");
+    }
+
     for (int i = 0; i < table[0].size(); ++i) {
-        for (const auto& column : table) {
-            file << column[i] << sep;
+        for (int j = 0; j < table.size(); ++j) {
+            file << table[j][i];
+            if (j != table.size() - 1) {
+                file << sep;
+            }
         }
         file << '\n';
     }
@@ -56,10 +62,10 @@ void csv_mouse::save_table(std::string filename) const {
     file.close();
 }
 
-void csv_mouse::check_row_size(const std::vector<std::string>& row) {
+void csv_mouse::check_row_size(const std::vector<std::string> &row) {
     if (row.size() != table.size()) {
         throw std::invalid_argument(
-            "Row size does not match defined number of columns in the table: " + 
+            "Row size does not match defined number of columns in the table: " +
             std::to_string(row.size()) + " != " + std::to_string(table.size())
         );
     }
